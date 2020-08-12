3aa0daec491fea835e40cee9d0415dbd7084e14e
James Strachan <james.strachan@gmail.com>
2020-08-12 07:10:38 +0100

    fix: add git credentail generator
    so we can commit and push to git in the git operator Job by default

--- pkg/cmd/git/setup/setup.go
+++ pkg/cmd/git/setup/setup.go
@@ -1,10 +1,23 @@
 package setup
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
+	"github.com/jenkins-x/jx-helpers/pkg/files"
 	"github.com/jenkins-x/jx-helpers/pkg/gitclient"
 	"github.com/jenkins-x/jx-helpers/pkg/gitclient/cli"
+	"github.com/jenkins-x/jx-helpers/pkg/gitclient/credentialhelper"
+	"github.com/jenkins-x/jx-helpers/pkg/homedir"
+	"github.com/jenkins-x/jx-helpers/pkg/kube"
+	"github.com/jenkins-x/jx-helpers/pkg/termcolor"
+	"github.com/jenkins-x/jx-logging/pkg/log"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
 	"github.com/jenkins-x/jx-gitops/pkg/rootcmd"
@@ -29,11 +42,16 @@ This is typically used in a pipeline to ensure git can do commits.
 
 // Options the options for the command
 type Options struct {
-	Dir           string
-	UserName      string
-	UserEmail     string
-	CommandRunner cmdrunner.CommandRunner
-	gitClient     gitclient.Interface
+	Dir                  string
+	UserName             string
+	UserEmail            string
+	OutputFile           string
+	DisableInClusterTest bool
+	Namespace            string
+	SecretName           string
+	KubeClient           kubernetes.Interface
+	CommandRunner        cmdrunner.CommandRunner
+	gitClient            gitclient.Interface
 }
 
 // NewCmdGitSetup creates a command object for the command
@@ -53,6 +71,9 @@ func NewCmdGitSetup() (*cobra.Command, *Options) {
 	cmd.Flags().StringVarP(&o.Dir, "dir", "d", "", "the directory to run the git push command from")
 	cmd.Flags().StringVarP(&o.UserName, "name", "n", "", "the git user name to use if one is not setup")
 	cmd.Flags().StringVarP(&o.UserEmail, "email", "e", "", "the git user email to use if one is not setup")
+	cmd.Flags().StringVarP(&o.OutputFile, "credentials-file", "", "", "The destination of the git credentials file to generate. If not specified uses $XDG_CONFIG_HOME/git/credentials or $HOME/git/credentials")
+	cmd.Flags().StringVarP(&o.Namespace, "namespace", "", "", "the namespace used to find the git operator secret for the git repository if running in cluster. Defaults to the current namespace")
+	cmd.Flags().StringVarP(&o.SecretName, "secret", "", "jx-boot", "the name of the Secret to find the git URL, username and password for creating a git credential if running inside the cluster")
 	return cmd, o
 }
 
@@ -67,6 +88,10 @@ func (o *Options) Run() error {
 	if err != nil {
 		return errors.Wrapf(err, "failed to setup credential store")
 	}
+
+	if o.DisableInClusterTest || IsInCluster() {
+		return o.GenerateGitCredentials()
+	}
 	return nil
 }
 
@@ -77,8 +102,122 @@ func (o *Options) GitClient() gitclient.Interface {
 	return o.gitClient
 }
 
+func (o *Options) GenerateGitCredentials() error {
+	credentials, err := o.findCredentials()
+	if err != nil {
+		return errors.Wrap(err, "creating git credentials")
+	}
+
+	outFile, err := o.determineOutputFile()
+	if err != nil {
+		return errors.Wrap(err, "unable to determine for git credentials")
+	}
+
+	return o.createGitCredentialsFile(outFile, credentials)
+}
+
+// findCredentials detects the git operator secret so we have default credentials
+func (o *Options) findCredentials() ([]credentialhelper.GitCredential, error) {
+	var credentialList []credentialhelper.GitCredential
+
+	var err error
+	o.KubeClient, o.Namespace, err = kube.LazyCreateKubeClientAndNamespace(o.KubeClient, o.Namespace)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create kube client")
+	}
+	ns := o.Namespace
+	name := o.SecretName
+	secret, err := o.KubeClient.CoreV1().Secrets(ns).Get(name, metav1.GetOptions{})
+	if err != nil {
+		if !apierrors.IsNotFound(err) {
+			log.Logger().Warnf("could not find secret %s in namespace %s", name, ns)
+			return nil, nil
+		}
+		return nil, errors.Wrapf(err, "failed to find Secret %s in namespace %s", name, ns)
+	}
+	data := secret.Data
+	if data != nil {
+		gitURL := string(data["url"])
+		if gitURL == "" {
+			log.Logger().Warnf("secret %s in namespace %s does not have a url entry", name, ns)
+			return nil, nil
+		}
+		username := string(data["username"])
+		password := string(data["password"])
+		credential, err := credentialhelper.CreateGitCredentialFromURL(gitURL, username, password)
+		if err != nil {
+			return nil, errors.Wrapf(err, "invalid git auth information")
+		}
+		credentialList = append(credentialList, credential)
+	}
+	return credentialList, nil
+}
+
+func (o *Options) determineOutputFile() (string, error) {
+	outFile := o.OutputFile
+	if outFile == "" {
+		outFile = GitCredentialsFile()
+	}
+
+	dir, _ := filepath.Split(outFile)
+	if dir != "" {
+		err := os.MkdirAll(dir, files.DefaultDirWritePermissions)
+		if err != nil {
+			return "", err
+		}
+	}
+	return outFile, nil
+}
+
+// CreateGitCredentialsFileFromUsernameAndToken creates the git credentials into file using the provided username, token & url
+func (o *Options) createGitCredentialsFile(fileName string, credentials []credentialhelper.GitCredential) error {
+	data, err := o.GitCredentialsFileData(credentials)
+	if err != nil {
+		return errors.Wrap(err, "creating git credentials")
+	}
+
+	if err := ioutil.WriteFile(fileName, data, files.DefaultDirWritePermissions); err != nil {
+		return fmt.Errorf("failed to write to %s: %s", fileName, err)
+	}
+	log.Logger().Infof("Generated Git credentials file %s", termcolor.ColorInfo(fileName))
+	return nil
+}
+
+// GitCredentialsFileData takes the given git credentials and writes them into a byte array.
+func (o *Options) GitCredentialsFileData(credentials []credentialhelper.GitCredential) ([]byte, error) {
+	var buffer bytes.Buffer
+	for _, gitCredential := range credentials {
+		u, err := gitCredential.URL()
+		if err != nil {
+			log.Logger().Warnf("Ignoring incomplete git credentials %q", gitCredential)
+			continue
+		}
+
+		buffer.WriteString(u.String() + "\n")
+		// Write the https protocol in case only https is set for completeness
+		if u.Scheme == "http" {
+			u.Scheme = "https"
+			buffer.WriteString(u.String() + "\n")
+		}
+	}
+
+	return buffer.Bytes(), nil
+}
+
 // IsInCluster tells if we are running incluster
 func IsInCluster() bool {
 	_, err := rest.InClusterConfig()
 	return err == nil
+}
+
+// GitCredentialsFile returns the location of the git credentials file
+func GitCredentialsFile() string {
+	cfgHome := os.Getenv("XDG_CONFIG_HOME")
+	if cfgHome == "" {
+		cfgHome = homedir.HomeDir()
+	}
+	if cfgHome == "" {
+		cfgHome = "."
+	}
+	return filepath.Join(cfgHome, "git", "credentials")
 }
--- pkg/cmd/git/setup/setup_test.go
+++ pkg/cmd/git/setup/setup_test.go
@@ -1,11 +1,17 @@
 package setup_test
 
 import (
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"github.com/jenkins-x/jx-gitops/pkg/cmd/git/setup"
 	"github.com/jenkins-x/jx-helpers/pkg/cmdrunner/fakerunner"
+	"github.com/jenkins-x/jx-helpers/pkg/testhelpers"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
 func TestGitSetup(t *testing.T) {
@@ -15,8 +21,31 @@ func TestGitSetup(t *testing.T) {
 	o.CommandRunner = runner.Run
 	o.UserEmail = "fakeuser@googlegroups.com"
 	o.UserName = "fakeusername"
+	o.DisableInClusterTest = true
 
-	err := o.Run()
+	ns := "jx"
+
+	o.Namespace = ns
+	o.KubeClient = fake.NewSimpleClientset(
+		&corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "jx-boot",
+				Namespace: ns,
+			},
+			Data: map[string][]byte{
+				"url":      []byte("https://github.com/myorg/myrepo.git"),
+				"username": []byte("myuser"),
+				"password": []byte("mypwd"),
+			},
+		},
+	)
+	tmpFile, err := ioutil.TempFile("", "")
+	require.NoError(t, err, "failed to create temp flie")
+	o.OutputFile = tmpFile.Name()
+
+	t.Logf("creating git credentials file %s", o.OutputFile)
+
+	err = o.Run()
 	require.NoError(t, err, "failed to run git setup")
 
 	runner.ExpectResults(t,
@@ -36,4 +65,6 @@ func TestGitSetup(t *testing.T) {
 			CLI: "git config --global credential.helper store",
 		},
 	)
+
+	testhelpers.AssertTextFilesEqual(t, filepath.Join("test_data", "expected.txt"), o.OutputFile, "generated git credentials file")
 }
