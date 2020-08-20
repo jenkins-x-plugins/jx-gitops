26068fea0394d4594104c6bb041cf27cae759fc3
James Strachan <james.strachan@gmail.com>
2020-08-20 04:54:29 +0100

    chore: stop PR'ing the version stream on release
    we will move that to the jx-cli PR

--- promote.sh
+++ promote.sh
@@ -4,8 +4,3 @@ echo "promoting the new version ${VERSION} to downstream repositories"
 
 jx step create pr regex --regex '\s+GitOpsVersion = "(?P<version>.*)"' --version ${VERSION} --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git
 
-jx step create pr regex \
-    --regex 'version: (.*)' \
-    --version ${VERSION} \
-    --files docker/gcr.io/jenkinsxio-labs-private/jx-gitops.yml \
-    --repo https://github.com/jenkins-x/jxr-versions.git
