8177a539cb13f6f08d0625e34889bd508638fb76
James Strachan <james.strachan@gmail.com>
2020-08-21 13:38:20 +0100

    fix: add test for: jx gitops helmfile template

---
+++ pkg/cmd/helmfile/template/template_test.go
@@ -0,0 +1,68 @@
+package template_test
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/jenkins-x/jx-gitops/pkg/cmd/helmfile/template"
+	"github.com/jenkins-x/jx-helpers/pkg/cmdrunner"
+	"github.com/jenkins-x/jx-helpers/pkg/files"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStepHelmfileTemplateFailsIfNoHelmfile(t *testing.T) {
+	_, o := template.NewCmdHelmfileTemplate()
+	o.Helmfile = filepath.Join("test_data", "does-not-exist.yaml")
+
+	err := o.Run()
+	require.Error(t, err, "should have failed due to missing helmfile")
+}
+
+func TestStepHelmfileTemplate(t *testing.T) {
+	skipTestIfCommandFails(t, "helm", "version")
+	skipTestIfCommandFails(t, "helmfile", "version")
+
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err, "failed to create tmp dir")
+
+	srcDir := filepath.Join("test_data")
+	require.DirExists(t, srcDir)
+
+	err = files.CopyDirOverwrite(srcDir, tmpDir)
+	require.NoError(t, err, "failed to copy generated crds at %s to %s", srcDir, tmpDir)
+
+	_, o := template.NewCmdHelmfileTemplate()
+	o.Dir = tmpDir
+	o.Args = "--include-crds"
+
+	err = o.Run()
+	require.NoError(t, err, "failed to run the command")
+
+	t.Logf("generated files to %s\n", o.OutputDir)
+
+	assert.FileExists(t, filepath.Join(o.OutputDir, "namespaces", "jx", "tekton", "controller.yaml"), "expected generated file")
+	assert.FileExists(t, filepath.Join(o.OutputDir, "namespaces", "secret-infra", "kubernetes-external-secrets", "deployment.yaml"), "expected generated file")
+	assert.FileExists(t, filepath.Join(o.OutputDir, "namespaces", "jx-production", "kubernetes-external-secrets", "deployment.yaml"), "expected generated file")
+
+	assert.FileExists(t, filepath.Join(o.OutputDir, "cluster", "namespaces", "jx.yaml"), "expected generated namespace")
+	assert.FileExists(t, filepath.Join(o.OutputDir, "cluster", "namespaces", "jx-production.yaml"), "expected generated namespace")
+	assert.FileExists(t, filepath.Join(o.OutputDir, "cluster", "namespaces", "secret-infra.yaml"), "expected generated namespace")
+
+	assert.FileExists(t, filepath.Join(o.OutputDir, "cluster", "secret-infra", "kubernetes-external-secrets", "kubernetes-client.io_externalsecrets_crd.yaml"), "expected generated CRD file")
+}
+
+func skipTestIfCommandFails(t *testing.T, name string, args ...string) {
+	c := &cmdrunner.Command{
+		Name: name,
+		Args: args,
+	}
+	text, err := cmdrunner.DefaultCommandRunner(c)
+	t.Logf("ran: %s\ngot: %s\n", c.CLI(), text)
+
+	if err != nil {
+		t.Logf("skipping the test as got %s\n", err.Error())
+		t.SkipNow()
+	}
+}
