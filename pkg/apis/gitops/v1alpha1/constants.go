3e26664f2ee7ac5ebc62650e41b5cf15b081dd5a
James Strachan <james.strachan@gmail.com>
2020-08-26 11:39:01 +0100

    chore: fmt

--- pkg/apis/gitops/v1alpha1/constants.go
+++ pkg/apis/gitops/v1alpha1/constants.go
@@ -5,7 +5,7 @@ const (
 	APIVersion = "gitops.jenkins-x.io/v1alpha1"
 
 	// KindSecretMapping the kind
-	KindSecretMapping= "SecretMapping"
+	KindSecretMapping = "SecretMapping"
 
 	// KindSourceConfig the kind
 	KindSourceConfig = "SourceConfig"
