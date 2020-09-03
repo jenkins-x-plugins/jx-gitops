7c7b66fa0caafe20c386b3059101d1e3c5ef4077
jstrachan <james.strachan@gmail.com>
2020-09-03 15:46:11 +0100

    fix: lets default the pipeline user/email properly if not specified

--- pkg/cmd/requirement/resolve/resolve_test.go
+++ pkg/cmd/requirement/resolve/resolve_test.go
@@ -13,6 +13,9 @@ import (
 	"github.com/jenkins-x/jx-helpers/pkg/httphelpers"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
 const (
@@ -26,7 +29,7 @@ var mockHeaders = map[string]string{
 	"Metadata-Flavor": "Google",
 }
 
-func TestRequirementsResolve(t *testing.T) {
+func TestRequirementsResolveGKE(t *testing.T) {
 	// lets mock the http requests...
 	client := httphelpers.GetClient()
 	gock.InterceptClient(client)
@@ -69,7 +72,7 @@ func TestRequirementsResolve(t *testing.T) {
 	tmpDir, err := ioutil.TempDir("", "")
 	require.NoError(t, err, "could not create temp dir")
 
-	srcFile := filepath.Join("test_data")
+	srcFile := filepath.Join("test_data", "gke")
 	require.DirExists(t, srcFile)
 
 	err = files.CopyDirOverwrite(srcFile, tmpDir)
@@ -82,6 +85,8 @@ func TestRequirementsResolve(t *testing.T) {
 
 	runner := &fakerunner.FakeRunner{}
 	o.CommandRunner = runner.Run
+	o.KubeClient = fake.NewSimpleClientset()
+	o.Namespace = "jx"
 
 	err = o.Run()
 	require.NoError(t, err, "failed to run git setup")
@@ -96,4 +101,55 @@ func TestRequirementsResolve(t *testing.T) {
 	assert.Equal(t, expectedProjectNumber, requirements.Cluster.GKEConfig.ProjectNumber, "requirements.Cluster.GKEConfig.ProjectNumber for file %s", fileName)
 	assert.Equal(t, expectedClusterName, requirements.Cluster.ClusterName, "requirements.Cluster.ClusterName for file %s", fileName)
 	assert.Equal(t, expectedLocation, requirements.Cluster.Zone, "requirements.Cluster.Zone for file %s", fileName)
+}
+
+func TestRequirementsResolvePipelineUser(t *testing.T) {
+	// setup the disk
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err, "could not create temp dir")
+
+	srcFile := filepath.Join("test_data", "eks")
+	require.DirExists(t, srcFile)
+
+	err = files.CopyDirOverwrite(srcFile, tmpDir)
+	require.NoError(t, err, "failed to copy %s to %s", srcFile, tmpDir)
+
+	// now lets run the command
+	_, o := resolve.NewCmdRequirementsResolve()
+	o.Dir = tmpDir
+	o.NoInClusterCheck = true
+
+	runner := &fakerunner.FakeRunner{}
+	o.CommandRunner = runner.Run
+
+	expectedPipelineEmail := "jenkins-x@googlegroups.com"
+	expectedPipelineUser := "myuser"
+
+	ns := "jx"
+	o.Namespace = ns
+	o.KubeClient = fake.NewSimpleClientset(
+		&corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "jx-boot",
+				Namespace: ns,
+			},
+			Data: map[string][]byte{
+				"email":    []byte(expectedPipelineEmail),
+				"username": []byte(expectedPipelineUser),
+			},
+		},
+	)
+	err = o.Run()
+	require.NoError(t, err, "failed to run git setup")
+
+	requirements, fileName, err := config.LoadRequirementsConfig(tmpDir, false)
+	require.NoError(t, err, "failed to load requirements from %s", tmpDir)
+	require.NotNil(t, requirements, "no requirements loaded in dir %s, tmpDir")
+	pipelineUser := requirements.PipelineUser
+	require.NotNil(t, pipelineUser, "no requirements.PipelineUser loaded in dir %s, tmpDir")
+
+	t.Logf("modified file %s\n", fileName)
+
+	assert.Equal(t, expectedPipelineUser, pipelineUser.Username, "requirements.PipelineUser.Username for file %s", fileName)
+	assert.Equal(t, expectedPipelineEmail, pipelineUser.Email, "requirements.PipelineUser.Email for file %s", fileName)
 }
