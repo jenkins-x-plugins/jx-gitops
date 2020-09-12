cdfe44b6a5c85127092583c51a609cbd0aa74bb7
James Strachan <james.strachan@gmail.com>
2020-09-12 06:40:07 +0100

    fix: switch to the git merge strategy
    to avoid losing changes

--- pkg/cmd/kpt/update/update.go
+++ pkg/cmd/kpt/update/update.go
@@ -70,7 +70,7 @@ func NewCmdKptUpdate() (*cobra.Command, *Options) {
 func (o *Options) AddFlags(cmd *cobra.Command) {
 	cmd.Flags().StringVarP(&o.Dir, "dir", "", ".", "the directory to recursively look for the *.yaml or *.yml files")
 	cmd.Flags().StringVarP(&o.Version, "version", "v", "master", "the git version of the kpt package to upgrade to")
-	cmd.Flags().StringVarP(&o.Strategy, "strategy", "s", "resource-merge", "the 'kpt pkg update' strategy to use")
+	cmd.Flags().StringVarP(&o.Strategy, "strategy", "s", "alpha-git-patch", "the 'kpt pkg update' strategy to use")
 	cmd.Flags().StringVarP(&o.RepositoryURL, "url", "u", "", "filter on the Kptfile repository URL for which packages to update")
 	cmd.Flags().StringVarP(&o.RepositoryOwner, "owner", "o", "", "filter on the Kptfile repository owner (user/organisation) for which packages to update")
 	cmd.Flags().StringVarP(&o.RepositoryName, "repo", "r", "", "filter on the Kptfile repository name  for which packages to update")
