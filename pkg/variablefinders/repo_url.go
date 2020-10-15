fddc5c4929f941caeba8b0d2cf1aec196691ba1d
James Strachan <james.strachan@gmail.com>
2020-10-15 17:14:50 +0100

    chore: fix failing release test

--- pkg/variablefinders/repo_url.go
+++ pkg/variablefinders/repo_url.go
@@ -9,11 +9,12 @@ import (
 
 // FindRepositoryURL finds the chart repository URL via environment variables or the dev Environment CRD
 func FindRepositoryURL(jxClient jxc.Interface, ns string, requirements *config.RequirementsConfig) (string, error) {
-	answer := os.Getenv("JX_CHART_REPOSITORY")
+	answer := ""
+	if requirements != nil {
+		answer = requirements.Cluster.ChartRepository
+	}
 	if answer == "" {
-		if requirements != nil {
-			answer = requirements.Cluster.ChartRepository
-		}
+		answer = os.Getenv("JX_CHART_REPOSITORY")
 	}
 	if answer == "" {
 		// assume default chart museum
