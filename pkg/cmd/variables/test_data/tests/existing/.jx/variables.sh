666040c3ea39b53600157deadbc972abd2437f9a
Cameron Braid <cameron@drivenow.com.au>
2021-01-29 13:13:34 +1100

    fix: support $ character in variable values
    When generating variables.sh use a single quot ' so that the shell doesn't interpret the value.  This allows variable values like 'robot$jx3'

--- pkg/cmd/variables/test_data/tests/existing/.jx/variables.sh
+++ pkg/cmd/variables/test_data/tests/existing/.jx/variables.sh
@@ -1,3 +1,3 @@
-export DOCKER_REGISTRY="something.else"
-export DOCKER_REGISTRY_ORG="another"
-export KANIKO_FLAGS="custom-stuff"
+export DOCKER_REGISTRY='something.else'
+export DOCKER_REGISTRY_ORG='another'
+export KANIKO_FLAGS='custom-stuff'
