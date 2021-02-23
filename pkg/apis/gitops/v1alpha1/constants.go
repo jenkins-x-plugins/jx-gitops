d2c7fedf24227c7f75a9687ceabc201ab36a5f27
James Strachan <james.strachan@gmail.com>
2021-02-23 05:49:52 +0000

    fix: lets default to use a host for generated Ingress
    to avoid the first boot job failing if using kapp
    
    we can replace change.me later on during the second boot

--- pkg/apis/gitops/v1alpha1/constants.go
+++ pkg/apis/gitops/v1alpha1/constants.go
@@ -9,4 +9,9 @@ const (
 
 	// KindSourceConfig the kind
 	KindSourceConfig = "SourceConfig"
+
+	// DomainPlaceholder what is the default domain value used as a place holder until
+	// the real domain name can be discovered which is usually after the first apply
+	// of kubernetes resources as we need to discover the LoadBalancer Service in the nginx namespace
+	DomainPlaceholder = "change.me"
 )
