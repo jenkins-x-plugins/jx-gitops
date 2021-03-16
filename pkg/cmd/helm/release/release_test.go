8e7e0a5fd20bc7df61fd19b1a4624f3079fd71d6
James Strachan <james.strachan@gmail.com>
2021-03-16 09:18:09 +0000

    fix: add an artifactory mode for releasing charts

--- pkg/cmd/helm/release/release_test.go
+++ pkg/cmd/helm/release/release_test.go
@@ -63,3 +63,50 @@ func TestStepHelmRelease(t *testing.T) {
 		t.Logf("ran: %s\n", c.CLI())
 	}
 }
+
+func TestStepHelmReleaseWithArtifactory(t *testing.T) {
+	runner := fakerunners.NewFakeRunnerWithGitClone()
+	helmBin := "helm"
+
+	ns := "jx"
+	devEnv := jxenv.CreateDefaultDevEnvironment(ns)
+	devEnv.Namespace = ns
+	devEnv.Spec.Source.URL = "https://github.com/jx3-gitops-repositories/jx3-kubernetes.git"
+
+	requirements := jxcore.NewRequirementsConfig()
+	requirements.Spec.Cluster.ChartRepository = "http://bucketrepo/bucketrepo/charts/"
+	data, err := yaml.Marshal(requirements)
+	require.NoError(t, err, "failed to marshal requirements")
+	devEnv.Spec.TeamSettings.BootRequirements = string(data)
+
+	jxClient := jxfake.NewSimpleClientset(devEnv)
+
+	_, o := release.NewCmdHelmRelease()
+	o.HelmBinary = helmBin
+	o.CommandRunner = runner.Run
+	o.ChartsDir = filepath.Join("test_data", "charts")
+	o.JXClient = jxClient
+	o.Namespace = ns
+	o.Version = "2.3.4"
+	o.Artifactory = true
+
+	o.KubeClient = fake.NewSimpleClientset(
+		&corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      kube.SecretJenkinsChartMuseum,
+				Namespace: ns,
+			},
+			Data: map[string][]byte{
+				"BASIC_AUTH_USER": []byte("myuser"),
+				"BASIC_AUTH_PASS": []byte("mypwd"),
+			},
+		},
+	)
+
+	err = o.Run()
+	require.NoError(t, err, "failed to run the command")
+
+	for _, c := range runner.OrderedCommands {
+		t.Logf("ran: %s\n", c.CLI())
+	}
+}
