57f0568410f32afc5a1d9df36fd20d04cf5cac3b
James Strachan <james.strachan@gmail.com>
2021-03-27 07:24:10 +0000

    fix: upgrade helm

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -19,7 +19,7 @@ const (
 	KappPluginName = "kapp"
 
 	// HelmVersion the default version of helm to use
-	HelmVersion = "3.5.1"
+	HelmVersion = "3.5.3"
 
 	// HelmfileVersion the default version of helmfile to use
 	HelmfileVersion = "0.138.2"
