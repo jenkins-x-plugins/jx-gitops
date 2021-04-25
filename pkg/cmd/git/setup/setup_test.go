1b7fae3d369b0252efa35c446c6078532ea6421d
James Strachan <james.strachan@gmail.com>
2021-04-25 07:39:52 +0100

    chore: add unit test for token with / character

--- pkg/cmd/git/setup/setup_test.go
+++ pkg/cmd/git/setup/setup_test.go
@@ -15,51 +15,70 @@ import (
 )
 
 func TestGitSetup(t *testing.T) {
-	_, o := setup.NewCmdGitSetup()
+	testCases := []struct {
+		username     string
+		password     string
+		expectedPath string
+	}{
+		{
+			username:     "myuser",
+			password:     "mypwd",
+			expectedPath: filepath.Join("test_data", "expected.txt"),
+		},
+		{
+			username:     "myuser",
+			password:     "my/pwd",
+			expectedPath: filepath.Join("test_data", "expected2.txt"),
+		},
+	}
 
-	runner := &fakerunner.FakeRunner{}
-	o.CommandRunner = runner.Run
-	o.UserEmail = "fakeuser@googlegroups.com"
-	o.DisableInClusterTest = true
+	for _, tc := range testCases {
+		_, o := setup.NewCmdGitSetup()
 
-	ns := "jx"
+		runner := &fakerunner.FakeRunner{}
+		o.CommandRunner = runner.Run
+		o.UserEmail = "fakeuser@googlegroups.com"
+		o.DisableInClusterTest = true
 
-	o.Namespace = ns
-	o.KubeClient = fake.NewSimpleClientset(
-		&corev1.Secret{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "jx-boot",
-				Namespace: ns,
+		ns := "jx"
+
+		o.Namespace = ns
+		o.KubeClient = fake.NewSimpleClientset(
+			&corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "jx-boot",
+					Namespace: ns,
+				},
+				Data: map[string][]byte{
+					"url":      []byte("https://github.com/myorg/myrepo.git"),
+					"username": []byte(tc.username),
+					"password": []byte(tc.password),
+				},
 			},
-			Data: map[string][]byte{
-				"url":      []byte("https://github.com/myorg/myrepo.git"),
-				"username": []byte("myuser"),
-				"password": []byte("mypwd"),
+		)
+		tmpFile, err := ioutil.TempFile("", "")
+		require.NoError(t, err, "failed to create temp flie")
+		o.OutputFile = tmpFile.Name()
+
+		t.Logf("creating git credentials file %s", o.OutputFile)
+
+		err = o.Run()
+		require.NoError(t, err, "failed to run git setup")
+
+		runner.ExpectResults(t,
+			fakerunner.FakeResult{
+				CLI: "git config --global --add user.name myuser",
 			},
-		},
-	)
-	tmpFile, err := ioutil.TempFile("", "")
-	require.NoError(t, err, "failed to create temp flie")
-	o.OutputFile = tmpFile.Name()
+			fakerunner.FakeResult{
+				CLI: "git config --global --add user.email fakeuser@googlegroups.com",
+			},
+			fakerunner.FakeResult{
+				CLI: "git config --global credential.helper store",
+			},
+		)
 
-	t.Logf("creating git credentials file %s", o.OutputFile)
-
-	err = o.Run()
-	require.NoError(t, err, "failed to run git setup")
-
-	runner.ExpectResults(t,
-		fakerunner.FakeResult{
-			CLI: "git config --global --add user.name myuser",
-		},
-		fakerunner.FakeResult{
-			CLI: "git config --global --add user.email fakeuser@googlegroups.com",
-		},
-		fakerunner.FakeResult{
-			CLI: "git config --global credential.helper store",
-		},
-	)
-
-	testhelpers.AssertTextFilesEqual(t, filepath.Join("test_data", "expected.txt"), o.OutputFile, "generated git credentials file")
+		testhelpers.AssertTextFilesEqual(t, tc.expectedPath, o.OutputFile, "generated git credentials file")
+	}
 }
 
 func TestGitSetupWithOperatorNamespace(t *testing.T) {
