7a83e6f21dca61558e727c264b69cf23cc244e1d
James Strachan <james.strachan@gmail.com>
2021-05-13 07:52:23 +0100

    chore: upgrade deps
    and improve test

--- pkg/cmd/repository/create/create_test.go
+++ pkg/cmd/repository/create/create_test.go
@@ -6,9 +6,16 @@ import (
 	"testing"
 
 	"github.com/jenkins-x-plugins/jx-gitops/pkg/cmd/repository/create"
+	v1 "github.com/jenkins-x/jx-api/v4/pkg/apis/jenkins.io/v1"
+	"github.com/jenkins-x/jx-api/v4/pkg/util"
 	"github.com/jenkins-x/jx-helpers/v3/pkg/files"
 	"github.com/jenkins-x/jx-helpers/v3/pkg/testhelpers"
 	"github.com/stretchr/testify/require"
+)
+
+var (
+	// generateTestOutput enable to regenerate the expected output
+	generateTestOutput = false
 )
 
 func TestCreateRepositorySourceDir(t *testing.T) {
@@ -34,8 +41,30 @@ func TestCreateRepositorySourceDir(t *testing.T) {
 	for _, name := range []string{"jenkins-x-jx-cli.yaml", "jenkins-x-jx-gitops.yaml", "mygitlaborg-somegitlab.yaml"} {
 		expectedFile := filepath.Join(expectedDir, name)
 		genFile := filepath.Join(genDir, name)
+
+		if generateTestOutput {
+			generatedFile := genFile
+			expectedPath := expectedFile
+			data, err := ioutil.ReadFile(generatedFile)
+			require.NoError(t, err, "failed to load %s", generatedFile)
+
+			err = ioutil.WriteFile(expectedPath, data, 0666)
+			require.NoError(t, err, "failed to save file %s", expectedPath)
+
+			t.Logf("saved file %s\n", expectedPath)
+			continue
+		}
+
 		testhelpers.AssertTextFilesEqual(t, expectedFile, genFile, "generated SourceRepository")
 
 		t.Logf("generated expected file %s\n", genFile)
+
+		target := &v1.SourceRepository{}
+		data, err := ioutil.ReadFile(genFile)
+		require.NoError(t, err, "failed to read file %s", genFile)
+
+		results, err := util.ValidateYaml(target, data)
+		require.NoError(t, err, "failed to validate file %s", genFile)
+		require.Empty(t, results, "should have validated file %s", genFile)
 	}
 }
